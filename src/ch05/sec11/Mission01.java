f8b52bf4d41ed4b10f5423341cc691b05cbd7da7
inha915 <dlsgk0915@gmail.com>
2025-05-30 17:24:25 +0900

    배열

---
+++ src/ch05/sec11/Mission01.java
@@ -0,0 +1,15 @@
+package ch05.sec11;
+
+public class Mission01 {
+    public static void main(String[] args) {
+        //args이용하여 각 방에 있는 값들을 모두 더한 값을 출력
+        int sum = 0;
+        int num = 0;
+
+        for(int i=0 ; i < args.length; i++){
+            num = Integer.parseInt(args[i]);
+            sum += num;
+        }
+        System.out.println(sum);
+    }
+}
